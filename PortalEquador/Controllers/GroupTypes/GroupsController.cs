bb51b1d7dbf52452562ef83bca20af3bd2db991d
gustavo <gustavo.isaac@gmail.com>
2022-09-10 22:32:24 +0100

    Control User Access with Role Authorization

--- PortalEquador/Controllers/GroupTypes/GroupsController.cs
+++ PortalEquador/Controllers/GroupTypes/GroupsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,7 @@ using PortalEquador.Models.GroupTypes;
 
 namespace PortalEquador.Controllers.GroupTypes
 {
+    [Authorize(Roles ="Administrator")]
     public class GroupsController : Controller
     {
         private readonly IGroupRepository groupRepository;
