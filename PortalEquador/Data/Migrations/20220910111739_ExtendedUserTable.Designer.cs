a8d9dd44905db53bab1b01e35b5b315720a455cb
gustavo <gustavo.isaac@gmail.com>
2022-09-10 12:32:57 +0100

    reating User Tables

--- PortalEquador/Data/Migrations/20220910111739_ExtendedUserTable.Designer.cs
+++ PortalEquador/Data/Migrations/20220910111739_ExtendedUserTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PortalEquador.Data;
 
@@ -11,9 +12,10 @@ using PortalEquador.Data;
 namespace PortalEquador.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220910111739_ExtendedUserTable")]
+    partial class ExtendedUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PortalEquador/Data/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PortalEquador.Data
+{
+    public class User : IdentityUser
+    {
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public DateTime DateJoined { get; set; }
+    }
+}
