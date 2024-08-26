a0635533a402197c69c7011288192e6f49eb410c
gustavo <gustavo.isaac@gmail.com>
2024-08-26 19:30:45 +0100

    GroupEntity

---
+++ PortalEquador/Data/GroupTypes/entities/GroupEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace PortalEquador.Data.GroupTypes.entities
+{
+    public class GroupEntity : BaseEntity
+    {
+        public required string Description { get; set; }
+
+        public string? Observation { get; set; }
+    }
+}
