b68aec1483c46d701a465ee5ecd74c92e03e6d00
gustavo <gustavo.isaac@gmail.com>
2024-08-26 19:26:59 +0100

    BaseEntity

---
+++ PortalEquador/Data/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortalEquador.Data
+{
+    public partial class BaseEntity
+    {
+        //[Key]
+        public int Id { get; set; }
+
+        public int EditorId { get; set; }
+
+        //[DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateCreated { get; set; }
+
+        public DateTime? DateModified { get; set; }
+    }
+}
