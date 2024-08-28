b17fb9514093ba71cbc68c17ec4295aa97b7c3d3
gustavo <gustavo.isaac@gmail.com>
2024-08-28 15:40:54 +0100

    CurriculumDashboardViewModel

---
+++ PortalEquador/Domain/Curriculum/ViewModels/CurriculumDashboardViewModel.cs
@@ -0,0 +1,21 @@
+﻿using PortalEquador.Domain.Generic;
+
+namespace PortalEquador.Domain.Curriculum.ViewModels
+{
+    public class CurriculumDashboardViewModel: ViewModel
+    {
+        public int Id { get; set; }
+
+        public required string FullName { get; set; }
+
+        public bool IsPersonalInformationComplete { get; set; }
+
+        public int TotalDocuments { get; set; }
+
+        public int TotalCompetences { get; set; }
+
+        public int TotalExpertises { get; set; }
+
+        public bool IsDriversLicenceComplete { get; set; }
+    }
+}
