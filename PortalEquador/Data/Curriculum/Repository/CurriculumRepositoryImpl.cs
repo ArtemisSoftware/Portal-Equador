9675cc51b68bef324356bfea216b38099f2c3de6
gustavo <gustavo.isaac@gmail.com>
2024-08-28 15:55:39 +0100

    CurriculumRepository

---
+++ PortalEquador/Data/Curriculum/Repository/CurriculumRepositoryImpl.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PortalEquador.Data.Curriculum.Entities;
+using PortalEquador.Data.Generic;
+using PortalEquador.Domain.Curriculum.Repository;
+using PortalEquador.Domain.Curriculum.ViewModels;
+using PortalEquador.Domain.GroupTypes.Repository;
+
+namespace PortalEquador.Data.Curriculum.Repository
+{
+    public class CurriculumRepositoryImpl(ApplicationDbContext context, IMapper mapper, IHttpContextAccessor httpContextAccessor) : GenericRepository<CurriculumEntity>(context, httpContextAccessor), CurriculumRepository
+    {
+        public async Task<CurriculumDashboardViewModel> GetCurriculumDashboard(int curriculumId)
+        {
+            return new CurriculumDashboardViewModel { FullName = "Lopes"};
+        }
+    }
+}
---
+++ PortalEquador/Domain/Curriculum/Repository/CurriculumRepository.cs
@@ -0,0 +1,11 @@
+﻿using PortalEquador.Data.Curriculum.Entities;
+using PortalEquador.Domain.Curriculum.ViewModels;
+using PortalEquador.Domain.Generic;
+
+namespace PortalEquador.Domain.Curriculum.Repository
+{
+    public interface CurriculumRepository : IGenericRepository<CurriculumEntity>
+    {
+        Task<CurriculumDashboardViewModel> GetCurriculumDashboard(int curriculumId);
+    }
+}
