5f1b492b52616346dce4d0ccad1eb51ca8f87de3
gustavo <gustavo.isaac@gmail.com>
2024-08-28 16:25:48 +0100

    PersonalInformationEntity

---
+++ PortalEquador/Data/PersonalInformation/PersonalInformationEntity.cs
@@ -0,0 +1,59 @@
+﻿using PortalEquador.Data.Generic;
+using PortalEquador.Data.GroupTypes.entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PortalEquador.Data.PersonalInformation
+{
+    public class PersonalInformationEntity : BaseEntity
+    {
+
+        /// <summary>
+        /// Numero do bilhete de identidade
+        /// </summary>
+        public string IdentityCard { get; set; }
+
+        public string Nif { get; set; }
+
+        public DateTime IdentityCardExpirationDate { get; set; }
+
+        public string? BeneficiaryNumber { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string? Email { get; set; }
+
+        public string Contacts { get; set; }
+
+        public int NationalityId { get; set; }
+
+        [ForeignKey("NationalityId")]
+        public GroupItemEntity NationalityGroupItemEntity { get; set; }
+
+        public int? ProvinceId { get; set; }
+
+        [ForeignKey("ProvinceId")]
+        public GroupItemEntity ProvinceGroupItemEntity { get; set; }
+
+        public int NeighbourhoodId { get; set; }
+
+        [ForeignKey("NeighbourhoodId")]
+        public GroupItemEntity NeighbourhoodGroupItemEntity { get; set; }
+
+        public string Address { get; set; }
+
+        public int? MotherTongueId { get; set; }
+
+        [ForeignKey("MotherTongueId")]
+        public GroupItemEntity MotherTongueGroupItemEntity { get; set; }
+
+        public int? MaritalStatusId { get; set; }
+
+        [ForeignKey("MaritalStatusId")]
+        public GroupItemEntity MaritalStatusIdGroupItemEntity { get; set; }
+
+    }
+}
