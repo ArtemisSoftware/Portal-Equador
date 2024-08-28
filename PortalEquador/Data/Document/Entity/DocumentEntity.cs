91c1d92fe4021e73c09a49de616dc72b1eb3d48c
gustavo <gustavo.isaac@gmail.com>
2024-08-28 18:28:52 +0100

    DocumentEntity

---
+++ PortalEquador/Data/Document/Entity/DocumentEntity.cs
@@ -0,0 +1,26 @@
+﻿using PortalEquador.Data.Generic;
+using PortalEquador.Data.GroupTypes.entities;
+using PortalEquador.Data.PersonalInformation.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PortalEquador.Data.Document.Entity
+{
+    public class DocumentEntity : BaseEntity
+    {
+
+        public int PersonalInformationId { get; set; }
+
+        [ForeignKey("PersonalInformationId")]
+        public PersonalInformationEntity PersonalInformationEntity { get; set; }
+
+        public string? Observation { get; set; }
+
+        public int DocumentTypeId { get; set; }
+
+        [ForeignKey("DocumentTypeId")]
+        public GroupItemEntity DocumentTypeGroupItemEntity { get; set; }
+
+        public required string Extension { get; set; }
+
+    }
+}
