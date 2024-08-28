20f3e8cfba5e7f86e61fb2b73f0910b41b8ddc2e
gustavo <gustavo.isaac@gmail.com>
2024-08-28 16:38:45 +0100

    CurriculumMapper

---
+++ PortalEquador/Data/Mappers/CurriculumMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PortalEquador.Data.PersonalInformation.Entity;
+using PortalEquador.Domain.GroupTypes.ViewModels;
+using PortalEquador.Domain.PersonalInformation.ViewModels;
+
+namespace PortalEquador.Data.Mappers
+{
+    public class CurriculumMapper : Profile
+    {
+        public CurriculumMapper()
+        {
+            CreateMap<PersonalInformationEntity, PersonalInformationViewModel>()
+                .ForMember(dest => dest.Editor, opt => opt.MapFrom(src => src.ApplicationUserEntity.FirstName + " " + src.ApplicationUserEntity.LastName))
+                .ReverseMap();
+        }
+    }
+}
