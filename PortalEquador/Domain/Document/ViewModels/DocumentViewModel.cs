381983c4acd9f048755d8381a50b8006885a70e2
gustavo <gustavo.isaac@gmail.com>
2024-08-28 18:32:01 +0100

    DocumentViewModel

---
+++ PortalEquador/Domain/Document/ViewModels/DocumentViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using PortalEquador.Domain.Generic;
+using PortalEquador.Domain.PersonalInformation.ViewModels;
+using PortalEquador.Util.Constants;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortalEquador.Domain.Document.ViewModels
+{
+    public class DocumentViewModel : ViewModel
+    {
+        public int Id { get; set; }
+
+        public int PersonaInformationId { get; set; }
+
+        public PersonalInformationViewModel? PersonalInformation { get; set; }
+
+        public string? Observation { get; set; }
+
+        public string? Extension { get; set; }
+
+
+        [Display(Name = StringConstants.Display.DOCUMENT)]
+        [Required]
+        public int DocumentTypeId { get; set; }
+
+        public SelectList? DocumentTypes { get; set; }
+
+        [Display(Name = StringConstants.Display.FILE)]
+        [Required(ErrorMessage = StringConstants.Error.MANDATORY_FILE)]
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+    }
+}
