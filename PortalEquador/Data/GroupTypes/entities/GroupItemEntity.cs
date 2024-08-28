b4cbe2324ab7206115ac43a37740ae40f09fa49f
gustavo <gustavo.isaac@gmail.com>
2024-08-28 11:28:08 +0100

    GroupItemEntity and GroupItemViewModel

---
+++ PortalEquador/Data/GroupTypes/entities/GroupItemEntity.cs
@@ -0,0 +1,21 @@
+﻿using PortalEquador.Data.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace PortalEquador.Data.GroupTypes.entities
+{
+    public class GroupItemEntity : BaseEntity
+    {
+        public required string Description { get; set; }
+
+        public string? Observation { get; set; }
+
+        [DefaultValue(true)]
+        public bool Active { get; set; } = true;
+
+        public int GroupEntityId { get; set; }
+
+        [ForeignKey("GroupEntityId")]
+        public required GroupEntity GroupEntity { get; set; }
+    }
+}
---
+++ PortalEquador/Domain/GroupTypes/ViewModels/GroupItemViewModel.cs
@@ -0,0 +1,27 @@
+﻿using PortalEquador.Domain.Generic;
+using PortalEquador.Util.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortalEquador.Domain.GroupTypes.ViewModels
+{
+    public class GroupItemViewModel : ViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = StringConstants.Display.DESCRIPTION)]
+        [Required(ErrorMessage = StringConstants.Error.MANDATORY_FIELD)]
+        public required string Description { get; set; }
+
+        [Display(Name = StringConstants.Display.OBSERVATION)]
+        [MaxLength(StringConstants.Lenght.LENGHT_140, ErrorMessage = StringConstants.Error.EXCEEDED_MAX_CHARACTERS_LENGHT)]
+        public string? Observation { get; set; }
+
+        [Display(Name = StringConstants.Display.STATE)]
+        public bool Active { get; set; } = true;
+
+        public int GroupId { get; set; }
+
+        public required GroupViewModel Group { get; set; }
+
+    }
+}
