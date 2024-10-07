43df21cfbf7d9521c0992a779c17c2e3e5fd5efd
gustavo <gustavo.isaac@gmail.com>
2024-10-07 16:41:29 +0100

    CarWash view models

---
+++ PortalEquador/Domain/MechanicalWorkshop/CarWash/ViewModels/CarWashViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using PortalEquador.Domain.Generic;
+using PortalEquador.Domain.GroupTypes.ViewModels;
+using PortalEquador.Domain.MechanicalWorkshop.Vehicle.ViewModels;
+using PortalEquador.Util;
+using PortalEquador.Util.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortalEquador.Domain.MechanicalWorkshop.CarWash.ViewModels
+{
+    public class CarWashViewModel : ViewModel
+    {
+        public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = StringConstants.Dates.DD_MM_YYYY)]
+        public DateOnly ScheduleDate { get; set; }
+
+        [Required]
+        public int InterventionTimeId { get; set; }
+
+        [Display(Name = StringConstants.Display.SCHEDULE)]
+        public GroupItemViewModel? InterventionTime { get; set; }
+
+
+        [Display(Name = StringConstants.Display.VEHICLE)]
+        [Required]
+        public int VehicleId { get; set; }
+        public VehicleViewModel? Vehicle { get; set; }
+        public SelectList? Vehicles { get; set; }
+
+
+
+        private CarWashSchedulerType _scheduleType = CarWashSchedulerType.InSchedule;
+        public CarWashSchedulerType ScheduleType
+        {
+            get
+            {
+                if (ScheduleDate < TimeUtil.DateOnlyCurrent() && _scheduleType == CarWashSchedulerType.Free)
+                {
+                    return CarWashSchedulerType.BlockedFree;
+                }
+                else if (ScheduleDate < TimeUtil.DateOnlyCurrent() && _scheduleType == CarWashSchedulerType.InSchedule)
+                {
+                    return CarWashSchedulerType.BlockedDateInThePast;
+                }
+                else
+                {
+                    return _scheduleType;
+                }
+            }
+            set
+            {
+                _scheduleType = value;
+            }
+        }
+
+    }
+}
---
+++ PortalEquador/Domain/MechanicalWorkshop/CarWash/ViewModels/WashDayPlannerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using PortalEquador.Domain.Generic;
+using PortalEquador.Domain.GroupTypes.ViewModels;
+using PortalEquador.Util.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortalEquador.Domain.MechanicalWorkshop.CarWash.ViewModels
+{
+    public class WashDayPlannerViewModel : ViewModel
+    {
+        [Display(Name = StringConstants.Display.DATE)]
+        [DisplayFormat(DataFormatString = StringConstants.Dates.DD_MM_YYYY)]
+        [DataType(DataType.Date)]
+        public DateTime MainTime { get; set; } = DateTime.Now;
+
+        public List<GroupItemViewModel> Schedules { get; set; }
+    }
+}
