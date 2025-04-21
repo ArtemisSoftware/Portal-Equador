9e5532d0863ae4811f2363babb774ca64e01f569
gustavo <gustavo.isaac@gmail.com>
2025-04-21 07:54:31 +0100

    Production version

--- PortalEquador/Util/Constants/AppConstants.cs
+++ PortalEquador/Util/Constants/AppConstants.cs
@@ -3,8 +3,8 @@
     public static class AppConstants
     {
 
-        public const int DEBUG = 0;
-        public const int PRODUCTION = 1;
-        public const int ENVIRONMENT = DEBUG;
+        public const int DEBUG = 0; //Development
+        public const int PRODUCTION = 1; //Production
+        public const int ENVIRONMENT = PRODUCTION;
     }
 }
