2ef756124541995bd497d915d33030eec2ebc7b7
gustavo <gustavo.isaac@gmail.com>
2025-09-22 11:41:39 +0100

    Uniform entities

---
+++ PortalEquador/Data/Uniforms/Entities/WorkerUniformEntity.cs
@@ -0,0 +1,29 @@
+﻿using PortalEquador.Data.Generic;
+using PortalEquador.Data.GroupTypes.entities;
+using PortalEquador.Data.PersonalInformation.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PortalEquador.Data.Uniforms.Entities
+{
+    public class WorkerUniformEntity : BaseEntity
+    {
+        public int PersonalInformationId { get; set; }
+
+        [ForeignKey("PersonalInformationId")]
+        public PersonalInformationEntity PersonalInformationEntity { get; set; }
+
+        public int UniformId { get; set; }
+
+        [ForeignKey("UniformId")]
+        public GroupItemEntity UniformGroupItemEntity { get; set; }
+
+
+        public int Quantity { get; set; }
+
+        public string Size { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string? Observation { get; set; }
+    }
+}
